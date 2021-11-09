9e45ad9b6e4d5c6227c88c67db0393bdc3c6f384
Geodesir <camilo.calderon.mt@correo.usa.edu.co>
2021-11-08 22:36:08 -0500

    First Commit

---
+++ src/main/java/co/usa/ciclo3/ciclo3/repository/CategoryRepository.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.usa.ciclo3.ciclo3.repository;
+
+import co.usa.ciclo3.ciclo3.model.Category;
+import co.usa.ciclo3.ciclo3.repository.crud.CategoryCrudRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Temporal
+ */
+@Repository
+public class CategoryRepository {
+
+    @Autowired
+    private CategoryCrudRepository categoryCrudRepository;
+    
+    public List<Category> getAll(){
+        return (List<Category>) categoryCrudRepository.findAll();
+    }
+    
+    public Optional <Category> getCategory(int id){
+        return categoryCrudRepository.findById(id);
+    }
+    
+    public Category save(Category c){
+        return categoryCrudRepository.save(c);
+    }
+}
---
+++ src/main/java/co/usa/ciclo3/ciclo3/service/CategoryService.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.usa.ciclo3.ciclo3.service;
+
+import co.usa.ciclo3.ciclo3.model.Category;
+import co.usa.ciclo3.ciclo3.repository.CategoryRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Temporal
+ */
+
+@Service
+public class CategoryService {
+    
+    @Autowired
+    private CategoryRepository categoryRepository;
+    
+    public List <Category> getAll(){
+        
+        return categoryRepository.getAll();
+    }         
+    
+    public Optional <Category> getCategory (int id){
+        
+        return categoryRepository.getCategory(id);
+    }
+    
+    public Category save (Category c){
+        if (c.getId()==null){
+            return categoryRepository.save(c);
+        }else{
+            Optional <Category> caux = categoryRepository.getCategory(c.getId());
+            if (caux.isEmpty()){
+                return categoryRepository.save(c);
+            }
+            else{
+                return c;
+            }
+    }
+        
+    }
+}
